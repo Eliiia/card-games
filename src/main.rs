243a158579f38fd52d00a37637ecdc4a8afb00b9
Agerr <agerrrr@proton.me>
2023-02-13 23:00:00 +0200

    i dunno

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let x = get_user_input("User input: ".to_string());
+    println!("{}", x);
+}
+
+// Gets user input as a cool independent function
+fn get_user_input(inp: String) -> String {
+    print!("{}", inp);
+    io::stdout().flush();
+
+    let mut answer = String::new();
+    io::stdin().read_line(&mut answer).expect("Failed to read");
+
+    answer
 }
